b94869b0b8d362a4eb3131720cb26dc9b27676d0
agyanim <agyanimconsultancy@gmail.com>
2023-05-02 17:04:17 +0000

    evants emmitter

--- src/index.js
+++ src/index.js
@@ -1,27 +1,62 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const pgConnect = require("./pgConnection");
-
-const app = express();
+const EventEmitter=require("events")
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async(req, res) => {
-try {
-    const result= (await pgConnect.query("SELECT * FROM library_schema.primarymember_tbl where primemid=$1 ORDER BY primemid ASC",[8])).rows;
-    res.send(result);
-    console.log(result);
+const customEmitter= new EventEmitter()
 
-} catch (error) {
-    console.log(error.message);
-}   
-});
 
-pgConnect.connect((err) => {
-  !err
-    ? console.log("connection successful")
-    : console.log("connection failed");
-});
+
+customEmitter.on("response",()=>{
+  console.log("my response emmitter");
+})
+
+customEmitter.on("callOut",(name,age)=>{
+console.log("My name is "+name+" and I am "+age+" years old");
+})
+
+customEmitter.emit("response")
+customEmitter.emit("callOut","gideon",20)
+// console.log("first task");
+// setTimeout(()=>{
+//   console.log("second task");
+
+// },
+// 0)
+// console.log("third task");
+// console.log("third task");
+// console.log("third task");
+
+
+// setInterval(()=>{
+// console.log("second");
+// },2000)
+
+// console.log("I will run first");
+
+
+// app.get("/", async(req, res) => {
+// try {
+//     const result= (await pgConnect.query("SELECT * FROM library_schema.primarymember_tbl where primemid=$1 ORDER BY primemid ASC",[8])).rows;
+//     res.send(result);
+//     console.log(result);
+
+// } catch (error) {
+//     console.log(error.message);
+// }   
+// });
+
+// pgConnect.connect((err) => {
+//   !err
+//     ? console.log("connection successful")
+//     : console.log("connection failed");
+// });
+
+
+
 
 
 app.listen("5000", () => {
